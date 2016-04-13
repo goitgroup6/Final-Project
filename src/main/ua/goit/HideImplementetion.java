ab1260f8f694ea3028bc197742e14fe39b40fa3e
Ilia Vovk <vovkilia@gmail.com>
2016-04-13 10:23:44 +0300

    fp

---
+++ src/main/ua/goit/HideImplementetion.java
@@ -0,0 +1,13 @@
+public class HideImplementetion {
+
+    private Decimal decimal = new Decimal();
+
+    public String ToBinary(String number,int system){
+        return decimal.DecimalTo(decimal.ToDecimal(number,system),2);
+    }
+
+    // Ограничить ввод только двоичного числа
+    public String BinaryTo(String number,int system){
+        return decimal.DecimalTo(decimal.ToDecimal(number,2),system);
+    }
+}
--- src/main/ua/goit/Main.java
+++ src/main/ua/goit/Main.java
@@ -1,4 +1,18 @@
-package main.ua.goit;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+
+        HideImplementetion hideImplementation = new HideImplementetion();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        String inputNumber = scanner.nextLine();
+        System.out.println("В какой системе это число");
+        int system = scanner.nextInt();
+        String result = hideImplementation.ToBinary(inputNumber,system);
+        System.out.println(result);
+        System.out.println("Переводим назад из двоичной");
+        System.out.println(hideImplementation.BinaryTo(result,system));
+    }
 }
