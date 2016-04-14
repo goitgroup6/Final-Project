e43d510bb6c2e994cdb0d5e893931b6b579dd6ef
OlegVlasenko <vlasenko.inbox@gmail.com>
2016-04-14 22:55:48 +0300

    New realization

--- src/main/ua/goit/HideImplementation.java
+++ src/main/ua/goit/HideImplementation.java
@@ -1,15 +1,15 @@
 package main.ua.goit;
 
-public class HideImplementetion {
+public class HideImplementation {
 
-    private Decimal decimal = new Decimal();
+    private static Decimal decimal = new Decimal();
 
-    public String ToBinary(String number,int system){
+    public static String ToBinary(String number,int system){
         return decimal.DecimalTo(decimal.ToDecimal(number,system),2);
     }
 
     // Ограничить ввод только двоичного числа
-    public String BinaryTo(String number,int system){
+    public static String BinaryTo(String number,int system){
         return decimal.DecimalTo(decimal.ToDecimal(number,2),system);
     }
 }
--- src/main/ua/goit/Main.java
+++ src/main/ua/goit/Main.java
@@ -5,16 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        HideImplementetion hideImplementation = new HideImplementetion();
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число");
         String inputNumber = scanner.nextLine();
+
         System.out.println("В какой системе это число");
-        int system = scanner.nextInt();
-        String result = hideImplementation.ToBinary(inputNumber,system);
+        Integer system = scanner.nextInt();
+
+        String result = HideImplementation.ToBinary(inputNumber,system);
         System.out.println(result);
+
+        //check
+        String res1 = Converter.toBinary(inputNumber, system);
+        System.out.println("Converter.toBinary(" + inputNumber + ")=" + res1);
+
+        System.out.println("Converter.fromBinary(" + res1 + ")=" + Converter.fromBinary( res1 ,system));
+
         System.out.println("Переводим назад из двоичной");
-        System.out.println(hideImplementation.BinaryTo(result,system));
+        System.out.println(HideImplementation.BinaryTo(result,system));
     }
 }
